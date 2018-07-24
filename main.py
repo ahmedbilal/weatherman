c0f86617bbf46b55fcc78ea149b9724249eb2de3
Ahmed Bilal <bilal@ShafqatFarhan-Desktop-82.arbisoft.com>
2018-07-24 15:08:09 +0500

    part 2 added

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import os
 import sys
 
-def shortToLongDate(arg):
-    date = arg.split("/")
-    year = date[0]
-    month = date[1]
-    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    month = months[int(month) - 1]
-    return (year, month)
-
 class WeathermanEntries:
     def __init__(self):
         self.entries = {}
@@ -81,71 +73,67 @@ class WeathermanEntries:
             return (None, None)
 
     def highest_avg_temp(self, by = None):
-        _highest_temp_avg = 0.0
+        _highest_temp = 0.0
+        _highest_temp_pkt = ''
         found = False
-        n = 0
-        date = shortToLongDate(by)
-        year = date[0]
-        month = date[1]
+
         for e in wmentries.entries.keys():
-            if by is not None and (year not in e or month not in e):
+            if by is not None and by not in e:
                 continue
             for d in wmentries.entries[e]:
                 max_temp = d.get('Max TemperatureC')
-                if max_temp is not None and max_temp.isalnum():
-                    _highest_temp_avg = _highest_temp_avg + float(max_temp)
+                if max_temp is not None and max_temp.isalnum() \
+                and float(max_temp) > _highest_temp:
+                    _highest_temp = float(max_temp)
+                    _highest_temp_pkt = d.get('PKT') or d.get('PKST')
                     found = True
-                    n = n + 1
         if found:
-            return float(_highest_temp_avg / float(n))
+            return (_highest_temp, _highest_temp_pkt)
         else:
-            return None
+            return (None, None)
 
 
     def lowest_avg_temp(self, by = None):
-        _lowest_temp_avg = 0.0
+        _lowest_temp = 100
+        _lowest_temp_pkt = ''
         found = False
-        n = 0
-        date = shortToLongDate(by)
-        year = date[0]
-        month = date[1]
+
         for e in wmentries.entries.keys():
-            if by is not None and (year not in e or month not in e):
+            if by is not None and by not in e:
                 continue
             for d in wmentries.entries[e]:
                 min_temp = d.get('Min TemperatureC')
-                if min_temp is not None and min_temp.isalnum():
-                    _lowest_temp_avg = _lowest_temp_avg + float(min_temp)
+                if min_temp is not None and min_temp.isalnum() \
+                and float(min_temp) < _lowest_temp:
+                    _lowest_temp = float(min_temp)
+                    _lowest_temp_pkt = d.get('PKT') or d.get('PKST')
                     found = True
-                    n = n + 1
         if found:
-            return float(_lowest_temp_avg / float(n))
+            return (_lowest_temp, _lowest_temp_pkt)
         else:
-            return None
+            return (None, None)
 
 
     def avg_mean_humid(self, by = None):
-        _mean_humid_avg = 0
+        _most_humid = 0
+        _most_humid_pkt = ''
         found = False
-        n = 0
-        date = shortToLongDate(by)
-        year = date[0]
-        month = date[1]
         for e in wmentries.entries.keys():
-            if by is not None and (year not in e or month not in e):
+            if by is not None and by not in e:
                 continue
             for d in wmentries.entries[e]:
-                mean_humid = d.get('Mean Humidity')
-                if mean_humid is not None and mean_humid.isalnum():
-                    _mean_humid_avg = _mean_humid_avg + float(mean_humid)
+                most_humid = d.get('Max Humidity')
+                if most_humid is not None and most_humid.isalnum() \
+                and float(most_humid) > _most_humid:
+                    _most_humid = float(most_humid)
+                    _most_humid_pkt = d.get('PKT') or d.get('PKST')
                     found = True
-                    n = n + 1
 
         if found:
-            return _mean_humid_avg
+            return (_most_humid, _most_humid_pkt)
         else:
-            return None
-
+            return (None, None)
+    
 wmentries = WeathermanEntries()
     
 
@@ -171,7 +159,7 @@ def read_file(file):
     meta = f.readline().strip("\n").replace(", ", ",").split(",")
     entries = []
     for line in f.readlines():
-        l = line.strip("\n").split(",")
+        l = line.strip("\n").split(",");
         entries.append(read_to_weatherman_entry(meta, l))
     wmentries.set_entries(file, entries)
 
@@ -193,13 +181,6 @@ def main():
         print("Highest:", highest_temp[0], "on", highest_temp[1])
         print("Lowest:", lowest_temp[0], "on", lowest_temp[1])
         print("Humidity:", most_humid[0], "on", most_humid[1])
-    elif mode == '-a':
-        date = sys.argv[3]
-        highest_avg_temp = wmentries.highest_avg_temp(date)
-        lowest_avg_temp = wmentries.lowest_avg_temp(date)
-        avg_mean_humid = wmentries.avg_mean_humid(date)
-        print("Highest Average:", highest_avg_temp)
-        print("Lowest Average:", lowest_avg_temp)
-        print("Average Mean Humidity:", avg_mean_humid)
+
 
 main()
